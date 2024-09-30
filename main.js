2b6cf26fa6f66a10b44f15ba2b06bf0f07621fe3
Efestus31 <biagiohd@gmail.com>
2024-09-30 15:33:34 +0200

    turning a string into a number

--- main.js
+++ main.js
@@ -11,14 +11,17 @@
 //preparazione
 //dichiariamo la prima variabile
 let age;
+Number(age);
 //dichiariamo la seconda variabile
 let distance;
+Number(distance);
 //dichiaro e inizializzo la variabile  dove salvare il messaggio finale
 let message = 'Il prezzo per il biglietto è:';
 //dichiaro la costante prezzo x km
 const price_km = 0.21;
 //dichiaro e applico funzione  per variabile prezzo totale
 let price = distance * price_km;
+Number(price);
 
 
 
@@ -28,11 +31,11 @@ let price = distance * price_km;
 
 //seconda fase : raccogliamo i dati
 //utilizzo un prompt per salvare l'input inserito nella variabile
-age = prompt('Ciao, inserisci la tua età qui sotto:');
+age = Number(prompt('Ciao, inserisci la tua età qui sotto:'));
 //mostro in console l'input ricevuto
 console.log(age);
 //utilizzo un prompt per salvare l'input inserito nella variabile
-distance = prompt('Ciao, inserisci la distanza che vuoi percorrere:');
+distance = Number(prompt('Ciao, inserisci la distanza che vuoi percorrere:'));
 //mostro in console l'input ricevuto
 console.log(distance);
 
@@ -42,16 +45,17 @@ console.log(distance);
 //verificare che il viaggiatore sia minorenne
 if (age <= 17) {
     let price = price - (price * (20 / 100));
-    console.log(message, price);
+    console.log(message); console.log(parseFloat(price));
+    
 
 }
 //verificare che il viaggiatore sia over 65
 else if (age > 65) {
     let price = price - (price * (40 / 100));
-    console.log(message, price);
+    console.log(message); console.log(parseFloat(price));
 
 } else {
-    console.log(message, price);
+    console.log(message); console.log(parseFloat(price));
 }
 
 
