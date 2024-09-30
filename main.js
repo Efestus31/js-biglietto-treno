154b0487eef59d867764bea3fe5505cd0bbd2ac2
Efestus31 <biagiohd@gmail.com>
2024-09-30 15:00:19 +0200

    data elaboration section

--- main.js
+++ main.js
@@ -13,8 +13,18 @@
 let age;
 //dichiariamo la seconda variabile
 let distance;
-//dichiaro la variabile  dove salvare il messaggio finale
-let message;
+//dichiaro e inizializzo la variabile  dove salvare il messaggio finale
+let message = 'Il prezzo per il biglietto è:';
+//dichiaro la costante prezzo x km
+const price_km = 0.21;
+//dichiaro e applico funzione  per variabile prezzo totale
+let price = distance * price_km;
+
+
+
+
+
+
 
 //seconda fase : raccogliamo i dati
 //utilizzo un prompt per salvare l'input inserito nella variabile
@@ -27,6 +37,23 @@ distance = prompt('Ciao, inserisci la distanza che vuoi percorrere:');
 console.log(distance);
 
 
+
 //trza fase : elaborare i dati
+//verificare che il viaggiatore sia minorenne
+if (age <= 17) {
+    let price = price - (price * (20 / 100));
+    console.log(message, price);
+
+}
+//verificare che il viaggiatore sia over 65
+else if (age > 65) {
+    let price = price - (price * (40 / 100));
+    console.log(message, price);
+
+} else {
+    console.log(message, price);
+}
+
 
 //quarta fase : mostrare output in console
+console.log(message, price);
